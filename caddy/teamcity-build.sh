7b3703ad48ae1c1c8eb01885b2c48418cc66b6fd
Alex <alex.hill@gmail.com>
2020-08-18 11:55:10 +0100

    move to docker hub

--- caddy/teamcity-build.sh
+++ caddy/teamcity-build.sh
@@ -3,12 +3,12 @@ set -e
 
 GIT_ID=$(git rev-parse --short=7 HEAD)
 GIT_BRANCH=$(git symbolic-ref --short HEAD)
-REGISTRY=docker.montagu.dide.ic.ac.uk:5000
+ORG=vimc
 NAME=montagu-static
 
-APP_DOCKER_TAG=$REGISTRY/$NAME
-APP_DOCKER_COMMIT_TAG=$REGISTRY/$NAME:$GIT_ID
-APP_DOCKER_BRANCH_TAG=$REGISTRY/$NAME:$GIT_BRANCH
+APP_DOCKER_TAG=$ORG/$NAME
+APP_DOCKER_COMMIT_TAG=$ORG/$NAME:$GIT_ID
+APP_DOCKER_BRANCH_TAG=$ORG/$NAME:$GIT_BRANCH
 
 docker build --pull \
        --tag $APP_DOCKER_COMMIT_TAG \
--- nginx/build.sh
+++ nginx/build.sh
@@ -4,10 +4,10 @@ set -ex
 git_id=$(git rev-parse --short=7 HEAD)
 git_branch=$(git symbolic-ref --short HEAD)
 
-registry=docker.montagu.dide.ic.ac.uk:5000
+org=vimc
 name=montagu-static-reverse-proxy
 
-commit_tag=$registry/$name:$git_id
-branch_tag=$registry/$name:$git_branch
+commit_tag=$org/$name:$git_id
+branch_tag=$org/$name:$git_branch
 
 docker build -t $commit_tag -t $branch_tag .
