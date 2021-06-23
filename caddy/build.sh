00bb50fdc136add2a1d3cd9604f030fead73792f
Mark Woodbridge <m.woodbridge@imperial.ac.uk>
2021-06-23 10:42:21 +0100

    Migrate from TeamCity to Buildkite

---
+++ caddy/build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+GIT_ID=$(git rev-parse --short=7 HEAD)
+GIT_BRANCH=$(git symbolic-ref --short HEAD)
+ORG=vimc
+NAME=montagu-static
+
+APP_DOCKER_COMMIT_TAG=$ORG/$NAME:$GIT_ID
+APP_DOCKER_BRANCH_TAG=$ORG/$NAME:$GIT_BRANCH
+
+docker build --pull \
+  --tag "$APP_DOCKER_COMMIT_TAG" \
+  --tag "$APP_DOCKER_BRANCH_TAG" \
+  .
+
+docker push "$APP_DOCKER_BRANCH_TAG"
+docker push "$APP_DOCKER_COMMIT_TAG"
